12b0d8dc486d4f2bbda48f6955d761b48aac90d8
Harrison Burr s3708981 <s3708981@student.rmit.edu.au>
2020-05-11 16:42:56 +1000

    Added some more methods to player.h

--- Player.h
+++ Player.h
@@ -4,25 +4,33 @@
 #include "Tile.h"
 #include <array>
 #include <vector>
+#include "BoxLid.h"
 using std::array;
 using std::vector;
 
 class Player{
     public:
-        Player(std::string name);
+        Player(std::string name, BoxLid* boxLid);
         ~Player();
 
         //Returns players tiles to the box, and applies any outstanding points.
         void nextRound();
 
-        //True if the player has a complete row of tiles on their wall (NOT ON A PATTERN LINE)
+        //Calculates final points total for the game and applies it to the players point count.
+        void completeGame();
+        
+        //True if the player has a complete row of tiles on their wall, signaling the end of the game.
         bool completeRow();
 
         /**
          * Takes a vector of tiles and tries to place them on a pattern line. Returns false if the move was illegal.
          * This vector may contain the FirstPlayer tile.
+         * TODO: Check if that colour is on the wall already in that row as well as checking the pattern wall colour.
          **/
-        bool applyTiles(int patternLine, vector<Tile*> tiles);
+        bool applyTilesToPattern(int patternLine, vector<Tile*> tiles);
+
+        //Transfers tile from pattern line onto the wall. Returns false if the move wasn't valid.
+        bool transferTileOntoWall(int patternLine);
 
         //Returns the tile at the position provided along the wall. Nullptr if none present.
         Tile* getWallTile(int row, int column);
@@ -33,17 +41,24 @@ class Player{
         //Returns the tile at the position provided on the the players pattern line. Nullptr if none present.
         Tile* getPatternLineTile(int row, int index);
 
+        //Returns the total points the player has.
+        int getPlayerPoints();
+
     private: 
+        //Add a tile to the players floor. Any tiles that don't fit will be placed back in the bag.
+        void addTileToFloor(Tile* tile);
+        //Box lid to place tiles into once they are removed from play.
+        BoxLid* boxLid;
         //Players name.
         std::string name;
         //Players total points.
         int points;
-        //Array of tiles on the floor.
-        Tile** TileFloor[7];
-        //2D array of tiles on the wall.
-        Tile** TileWall[5][5];
-        //Array of pattern line arrays.
-        Tile** PatternLine[5];
+        //Array of pointers to tiles on the floor.
+        Tile** TileFloor;
+        //2D array of pointers to tiles on the wall.
+        Tile*** TileWall;
+        //Array of Pattern line arrays of tile pointers..
+        Tile*** PatternLine;
 };
 
 #endif // PLAYER_H
