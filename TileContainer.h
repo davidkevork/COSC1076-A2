dbc3f2ccaf7259df6db6911ca3813cb4c9327cff
Harrison Burr s3708981 <s3708981@student.rmit.edu.au>
2020-05-11 12:04:37 +1000

    Made TileContainer an abstract class.

--- TileContainer.h
+++ TileContainer.h
@@ -7,14 +7,14 @@
 class TileContainer{
     public:
         //Returns true if the tile container contains this specific tile.
-        bool contains(Tile* tile);
+        virtual bool contains(Tile* tile);
         //Returns true if the tile container contains a tile of the colour provided.
-        bool contains(Colour colour);
+        virtual bool contains(Colour colour);
 
         //Removes the specific tile from the container.
-        bool remove(Tile* tile);
+        virtual bool remove(Tile* tile);
         //Appends the specific tile to the container.
-        bool append(Tile* tile);
+        virtual bool append(Tile* tile);
 };
 
 #endif // TILE_CONTAINER_H
