a7fa273a9446430ddc93e10ad64734c8ed595990
Harrison Burr s3708981 <s3708981@student.rmit.edu.au>
2020-05-22 12:53:34 +1000

    Added command line options to menu.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,19 @@
 #include "Tile.h"
 #include <ctime>
 
-int main(int argc, char* args[]) {
-    Menu* menu = new Menu();
-    try{
-        srand(std::stoi(args[1]));
-    } catch (...) {
-        srand(std::time(0));
+int main(int argc, char* args[]){
+    if(std::string(args[1])=="--help"){
+        std::cout<<std::endl<<"Usage: Azul.exe [<SEED>]"<<std::endl<<"Seed determines the game's randomness, must be a number."<<std::endl;
+    } else if(std::string(args[1])=="--version"){
+        std::cout<<std::endl<<"Version 0.1"<<std::endl;
+    } else {
+        try{
+            srand(std::stoi(args[1]));
+        } catch (...) {
+            srand(std::time(0));
+        }
+        Menu* menu = new Menu();
+        menu->runMenu();
     }
-    menu->runMenu();
     return EXIT_SUCCESS;
 }
